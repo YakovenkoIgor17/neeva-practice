f33b73bacf5a0d5584d99064e78500e1bc8a59eb
Yakovenkoigor17 <godwares@gmail.com>
2021-07-19 12:36:52 +0300

    Begin-menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//click menu active
+$(document).ready(function () {
+	$('.header-menu__icon').click(function (event) {
+		$('.header-menu__icon, .header-menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
